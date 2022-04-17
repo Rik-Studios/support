9e4f2c050d0983d0d408c454d9ebed34ea12e6bd
João Bomfim <oi@joaobomfim.com.br>
2022-04-17 00:10:49 -0300

    feat: add meta tags

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -15,6 +15,44 @@ export const Home = () => {
     <S.Wrapper>
       <Head>
         <title>🥦 Rik | Apoio</title>
+        <meta property='og:url' content='https://apoio.rik.net.br/' />
+        <meta property='og:type' content='tpage' />
+        <meta property='og:title' content='Rik - Formas de Apoio' />
+        <meta
+          property='og:image'
+          content='https://apoio.rik.net.br/assets/images/video/thumbnail-desktop.jpg'
+        />
+        <meta
+          property='og:description'
+          content='Saiba quais são as formas e as categorias de apoio'
+        />
+        <meta property='og:site_name' content='Rik' />
+        <meta
+          property='article:author'
+          content='https://www.facebook.com/RikStudios'
+        />
+        <meta
+          property='article:publisher'
+          content='https://www.facebook.com/RikStudios'
+        />
+        <meta
+          property='article:published_time'
+          content='momento de publicação'
+        />
+        <meta property='article:tag' content='tag1' />
+        <meta property='article:tag' content='tag2' />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@RikEditor' />
+        <meta name='twitter:title' content='Rik - Formas de Apoio' />
+        <meta
+          name='twitter:description'
+          content='Saiba quais são as formas e as categorias de apoio'
+        />
+        <meta
+          property='twitter:image:src'
+          content='https://apoio.rik.net.br/assets/images/video/thumbnail-desktop.jpg'
+        />
       </Head>
       <Header />
       <Feature />
