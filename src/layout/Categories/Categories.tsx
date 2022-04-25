08ded1d2e224184b6a7412e2f5071364755c9a30
João Bomfim <oi@joaobomfim.com.br>
2022-04-24 23:14:45 -0300

    feat: slick slide and responsive

--- src/layout/Categories/Categories.tsx
+++ src/layout/Categories/Categories.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import Slider from 'react-slick'
 
 import Card from '~/components/Card/Card'
 
@@ -10,6 +16,26 @@ import { CategoriesList } from './Categories.static'
 import * as S from './Categories.styles'
 
 const Categories = () => {
+  const theme = useTheme()
+  const displayMenu = useMediaQuery(theme.breakpoints.down('md'))
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  }
+
+  const cardItems = CategoriesList.map(
+    ({ title, emoj, price, benefits, isFeature }) => (
+      <Grid item xs={12} md={4} key={title}>
+        <Card {...{ title, emoj, price, benefits, isFeature }} />
+      </Grid>
+    )
+  )
+
   return (
     <>
       <Container>
@@ -23,25 +49,23 @@ const Categories = () => {
             <Grid item xs={12}>
               <S.Title> Categorias</S.Title>
             </Grid>
-            <Grid item xs={12}>
-              <Grid
-                container
-                spacing={2}
-                justifyContent='space-evenly'
-                alignItems='center'
-              >
-                {CategoriesList.map(
-                  ({ title, emoj, price, benefits, isFeature }) => (
-                    <Grid item xs={12} md={4} key={title}>
-                      <Card {...{ title, emoj, price, benefits, isFeature }} />
-                    </Grid>
-                  )
-                )}
-              </Grid>
-            </Grid>
           </Grid>
         </Box>
       </Container>
+      <S.WrapperSlide>
+        {displayMenu ? (
+          <Slider {...settings}>{cardItems}</Slider>
+        ) : (
+          <Grid
+            container
+            spacing={2}
+            justifyContent='center'
+            alignItems='center'
+          >
+            {cardItems}
+          </Grid>
+        )}
+      </S.WrapperSlide>
     </>
   )
 }
